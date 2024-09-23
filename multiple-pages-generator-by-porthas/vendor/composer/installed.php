8402842cae5ebbe7ca6ee2afc525460b00b7b1fa
Sean Murphy <sean@iamseanmurphy.com>
2024-09-23 17:40:18 +0000

    3.4.8

---
+++ multiple-pages-generator-by-porthas/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'codeinwp/multi-pages-plugin',
+        'pretty_version' => 'v3.4.8',
+        'version' => '3.4.8.0',
+        'reference' => 'ab2d080b91a2250b4ad0304d8afaacb17ab09742',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'codeinwp/multi-pages-plugin' => array(
+            'pretty_version' => 'v3.4.8',
+            'version' => '3.4.8.0',
+            'reference' => 'ab2d080b91a2250b4ad0304d8afaacb17ab09742',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'codeinwp/themeisle-sdk' => array(
+            'pretty_version' => '3.3.29',
+            'version' => '3.3.29.0',
+            'reference' => '4d63bc81ec0357c8675dac1e35511fd81104605d',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../codeinwp/themeisle-sdk',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
